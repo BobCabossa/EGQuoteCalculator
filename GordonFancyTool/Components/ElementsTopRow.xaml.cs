68e823f99b13a284adda43d7cf9d62a39df16039
BobCabossa <kasperklassen@gmail.com>
2026-09-09 15:17:40 +0200

    Begin making the elements

---
+++ GordonFancyTool/Components/ElementsTopRow.xaml.cs
@@ -0,0 +1,60 @@
+﻿namespace GordonFancyTool.Components;
+
+/// <summary>
+/// Interaction logic for Test.xaml
+/// </summary>
+public partial class ElementsTopRow : UserControl
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(ElementsTopRow));
+
+    public static readonly DependencyProperty Text1Property =
+        DependencyProperty.Register(
+            nameof(Text1), 
+            typeof(string), 
+            typeof(ElementsTopRow));
+
+    public static readonly DependencyProperty Text2Property =
+        DependencyProperty.Register(
+            nameof(Text2), 
+            typeof(string), 
+            typeof(ElementsTopRow));
+
+    public static readonly DependencyProperty Text3Property =
+        DependencyProperty.Register(
+            nameof(Text3),
+            typeof(string),
+            typeof(ElementsTopRow));
+
+    public ElementsTopRow()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        set => SetValue(TitleProperty, value);
+        get => (string)GetValue(TitleProperty);
+    }
+
+    public string Text1
+    {
+        set => SetValue(Text1Property, value);
+        get => (string)GetValue(Text1Property);
+    }
+
+    public string Text2
+    {
+        set => SetValue(Text2Property, value);
+        get => (string)GetValue(Text2Property);
+    }
+
+    public string Text3
+    {
+        set => SetValue(Text3Property, value);
+        get => (string)GetValue(Text3Property);
+    }
+}
--- GordonFancyTool/Pages/ElementPage.xaml.cs
+++ GordonFancyTool/Pages/ElementPage.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace GordonFancyTool.Pages;
 
-namespace GordonFancyTool.Pages
+/// <summary>
+/// Interaction logic for ElementPage.xaml
+/// </summary>
+public partial class ElementPage : Page
 {
-    /// <summary>
-    /// Interaction logic for ElementPage.xaml
-    /// </summary>
-    public partial class ElementPage : Page
+    public ElementPage()
     {
-        public ElementPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    public void BackToProject(object sender, RoutedEventArgs e)
+    {
+        NavigationService.Navigate(new ProjectPage());
     }
 }
